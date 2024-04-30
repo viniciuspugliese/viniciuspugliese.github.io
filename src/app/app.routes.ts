3ed12d4a1093dde24bbf12c4621b564f4c4cdcb8
viniciuspugliesi <vinicius.pugliesi@portoseguro.com.br>
2024-04-29 22:24:28 -0300

    Portfolio page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import {Routes} from '@angular/router';
 import {ContactComponent} from "./pages/contact/contact.component";
 import {AboutComponent} from "./pages/about/about.component";
+import {PortfolioComponent} from "./pages/portfolio/portfolio.component";
 
 export const routes: Routes = [
     {path: '', component: AboutComponent},
-    {path: 'contact', component: ContactComponent}
+    {path: 'contact', component: ContactComponent},
+    {path: 'works', component: PortfolioComponent},
 ];
